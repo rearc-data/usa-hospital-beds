257e09339ac487b46ea98a0e9c931f71340f41ec
Derek Wolpert <me@derekwolpert.com>
2020-04-27 12:07:15 -0400

    update pre-processing

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,16 +1,25 @@
 import os
-import pandas as pd
 import boto3
-from urllib.request import urlopen
-
-source_dataset_url = "https://opendata.arcgis.com/datasets/1044bb19da8d4dbfb6a96eb1b4ebf629_0.geojson"
+import urllib.request
 
 def source_dataset(new_filename, s3_bucket, new_s3_key):
-	url = urlopen(source_dataset_url)
-	output = open('/tmp/' + new_filename, 'wb')
-	output.write(url.read())
-	output.close()
 
-	#uploading new s3 dataset
+	source_dataset_url = 'https://opendata.arcgis.com/datasets/1044bb19da8d4dbfb6a96eb1b4ebf629_0'
+
+	# Download the file from `url` and save it locally under `file_name`:
+	urllib.request.urlretrieve(
+		source_dataset_url + '.geojson', '/tmp/' + new_filename + '.geojson')
+
+	urllib.request.urlretrieve(
+		source_dataset_url + '.csv', '/tmp/' + new_filename + '.csv')
+
 	s3 = boto3.client('s3')
-	s3.upload_file('/tmp/' + new_filename, s3_bucket, new_s3_key)
+	asset_list = []
+
+	for filename in os.listdir('/tmp'):
+		print(filename)
+		s3.upload_file('/tmp/' + filename, s3_bucket, new_s3_key + '.' + filename.rsplit('.', 1)[1])
+		asset_list.append(
+			{'Bucket': s3_bucket, 'Key': new_s3_key + '.' + filename.rsplit('.', 1)[1]})
+	
+	return asset_list
